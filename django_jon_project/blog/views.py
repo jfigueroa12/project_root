51ed350a1581da0903bcfb662b463081a8a264e2
flyasth3sky <jonathan.figueroa@zoho.com>
2018-12-09 21:37:06 -0500

    Added html files and views

--- django_jon_project/blog/views.py
+++ django_jon_project/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 # Create your views here.
 class BlogListView(ListView):
     model = Post
     template_name = 'django_blog_home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'django_blog_post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'django_blog_post_new.html'
+    fields = '__all__'
