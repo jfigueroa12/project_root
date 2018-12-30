306d2c04c059e631cb3413ee4d087efe86f21f95
flyasth3sky <jonathan.figueroa@zoho.com>
2018-12-29 22:09:31 -0500

    Added edit and delete functionality.  Also added unit tests

--- django_jon_project/blog/views.py
+++ django_jon_project/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
@@ -16,3 +17,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'django_blog_post_new.html'
     fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'django_blog_post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'django_blog_post_delete.html'
+    success_url = reverse_lazy('home')
