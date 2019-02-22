1d996be14b6242898546f05f610f67d24b64fe6e
System Administrator <root@lmis0682.mskcc.org>
2019-02-22 11:09:12 -0500

    Added templates and project_static folder

--- newspaper_project/newspaper_project/settings.py
+++ newspaper_project/newspaper_project/settings.py
@@ -122,6 +122,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+AWS_STORAGE_BUCKET_NAME =  os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+# Tell django-storages the domain to use to refer to static files.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+#AWS_LOCATION = 'static'
+
+# Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 STATIC_URL = '/static/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'project_static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project_static'),
+]
+
+# Customer user model
 
 AUTH_USER_MODEL = 'users.CustomUser'
