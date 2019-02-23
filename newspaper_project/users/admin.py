6eaa356edaffb2f3b27fd44c61b17ce6498fbeee
flyasth3sky <jonathan.figueroa@zoho.com>
2019-02-23 17:23:29 -0500

    Updated users/forms.py and users/admin.py

--- newspaper_project/users/admin.py
+++ newspaper_project/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from .forms import CustomerUserChangeForm, CustomerUserCreationForm
+from .models import CustomUser
+
+class CustomerUserAdmin(UserAdmin):
+    add_form = CustomerUserCreationForm
+    form = CustomerUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username', 'age', 'is_staff',]
+
+admin.site.register(CustomUser, CustomerUserAdmin)
